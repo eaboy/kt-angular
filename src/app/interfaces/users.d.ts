c516f20d8f2ca5da75a9f47a20671cba6d3d7556
Aboy <e.aboy@sinteac.es>
2018-04-15 20:51:25 +0200

    Crea los interfaces de users y articles

---
+++ src/app/interfaces/users.d.ts
@@ -0,0 +1,15 @@
+
+export declare interface User {
+    id_user?: number,
+    email: string,
+    image?: string,
+    instagram_user?: string,
+    twitter_user?: string,
+    facebook_user ?: string,
+    about_me?: string
+}
+
+export declare interface Login {
+    user: string,
+    password: string
+}
